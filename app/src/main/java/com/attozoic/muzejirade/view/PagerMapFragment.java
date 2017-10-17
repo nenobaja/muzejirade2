79c4f1c7d1ed774c67a732759c8189b192e03b40
DESKTOP-CQF1TSL\nenadicivan <nenadicivan86@gmail.com>
2017-10-18 00:33:01 +0200

    Some kind of map is ready!!!!

--- app/src/main/java/com/attozoic/muzejirade/view/PagerMapFragment.java
+++ app/src/main/java/com/attozoic/muzejirade/view/PagerMapFragment.java
@@ -10,12 +10,12 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.attozoic.muzejirade.R;
-import com.attozoic.muzejirade.model.iListItem;
+
 import com.attozoic.muzejirade.presenter.MapFragmentPresenter;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
+
+
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -26,10 +26,10 @@ import java.util.List;
  * Created by nenadicivan on 10/6/2017.
  */
 
-public class PagerMapFragment extends Fragment implements ListOfMuseumsInteface {
+public class PagerMapFragment extends Fragment implements ListOfMuseumsInteface,OnMapReadyCallback {
 
     TextView content;
-//   private GoogleMap mMap;
+   private GoogleMap mMap;
 
     private MapFragmentPresenter presenter;
     @Override
@@ -41,7 +41,7 @@ public class PagerMapFragment extends Fragment implements ListOfMuseumsInteface 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_pager_map, container, false);
-         //content = (TextView)v.findViewById(R.id.text_map);
+
 
          presenter = MapFragmentPresenter.getInstance();
          presenter.setiMuseumsMapView(this);
@@ -51,9 +51,17 @@ public class PagerMapFragment extends Fragment implements ListOfMuseumsInteface 
     }
 
     @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment fragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        fragment.getMapAsync(this);
+    }
+
+    @Override
     public void onActivityCreated(Bundle bundle) {
         super.onActivityCreated(bundle);
-//        getMapAsync(this);
+
     }
 
     @Override
@@ -63,11 +71,16 @@ public class PagerMapFragment extends Fragment implements ListOfMuseumsInteface 
 
 //    @Override
 //    public void onMapReady(GoogleMap googleMap) {
-//        mMap = googleMap;
 //
-//        // Add a marker in Sydney and move the camera
-//        LatLng sydney = new LatLng(-34, 151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 //    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
